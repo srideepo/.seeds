4faf357bd0c2465ec552326de0e0b2b2e1e99406
Sreedeep-Janardhanan <sreedeep.janardhanan@allscripts.com>
2025-02-23 20:46:12 -0500

    read from excel metadata

--- python-ddl-to-json/main.py
+++ python-ddl-to-json/main.py
@@ -11,10 +11,29 @@ https://pypi.org/project/simple-ddl-parser/
 
 from simple_ddl_parser import DDLParser
 import json
+import pandas as pd
 
-db_dialect = 'hql'
+db_dialect = 'mssql'
 ddl_file = 'dbschema.sql'
 json_file = 'dbschema.json'
+metadata_excel = 'DBSchema.xlsx'
+
+def metadata_to_ddl(_metadata_excel) -> str:
+    _ddl = ''
+    _prefix = ''
+    _PREFIX_COMMA = ','
+    _df_metadata = pd.read_excel(_metadata_excel, index_col=None, keep_default_na=False)
+    _df_metadata = _df_metadata.sort_values(by=['TABLE_SCHEMA', 'TABLE_NAME', 'ORDINAL_POSITION'])
+    _ddl += f"CREATE TABLE {_df_metadata.loc[0]['TABLE_SCHEMA']}.{_df_metadata.loc[0]['TABLE_NAME']}(\n"
+    for (_ix, _row) in _df_metadata.iterrows():
+        if _ix > 0 and _df_metadata.loc[_ix-1]['TABLE_NAME'] != _row['TABLE_NAME']:
+            _ddl += f");\nCREATE TABLE {_row['TABLE_SCHEMA']}.{_row['TABLE_NAME']}(\n"
+            _prefix = ''
+        _ddl += f"\t{_prefix}{_row['COLUMN_NAME']} {_row['DATA_TYPE']}{'' if len(str(_row['COLUMN_SIZE']))==0 else ''.join(['(', str(_row['COLUMN_SIZE']), ')']) } {_row['IDENTITY_DEFINITION']}{' NULL' if _row['IS_NULLABLE'].upper() == 'YES' else ' NOT NULL'} {'' if len(str(_row['COLUMN_DEFAULT']))==0 else ''.join([' DEFAULT(', str(_row['COLUMN_DEFAULT']), ')'])}\n"
+        _prefix = _PREFIX_COMMA
+    _ddl += ');'
+    print(_ddl)
+    return _ddl
 
 ddl = """
     CREATE TABLE IF NOT EXISTS default.salesorder(
@@ -50,9 +69,10 @@ ddl = """
 # read ddl from file
 #file = open(ddl_file, "r")
 #ddl = file.read()
+ddl = metadata_to_ddl(metadata_excel)
 
 result = DDLParser(ddl).run(output_mode=db_dialect)
-
+print(result)
 result_json = json.dumps(result, indent=4)
 
 # Writing json result to file
